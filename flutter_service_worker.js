f863f1af4ce9bf5fc074c2d3bfc45303ea35376a
shaik naser <naser.shaikmohammed@academy.com>
2025-11-18 11:36:22 +0530

    324

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -70,7 +70,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0f9059e88fc89b251f4f86d6ce5f8594
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "b1110d76b10c5edb906e6fb1fd3b6095",
+"flutter_bootstrap.js": "7634b0eb1b45a3c6d12c194a331b27c1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
